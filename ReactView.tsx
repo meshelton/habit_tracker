a50ed07ef532108747f616bf2a733abe65b935ee
Michael Shelton <michael.e.shelton@gmail.com>
2024-10-01 12:14:29 -0400

    Plugin rendering react nodes

---
+++ ReactView.tsx
@@ -0,0 +1,14 @@
+﻿import { Root } from "react-dom/client";
+import { StrictMode } from "react";
+
+export function TestRender(root: Root) {
+	root.render(
+		<StrictMode>
+			<ReactView />
+		</StrictMode>,
+	);
+}
+
+export const ReactView = () => {
+	return <h4>Hello, React!</h4>;
+};
